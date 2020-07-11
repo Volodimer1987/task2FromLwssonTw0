087605080101200bb583059fed6027019eb00e5a
Vladimir <kynicin87@mail.ru>
2020-07-11 10:11:25 +0300

    Initial Commit

---
+++ task2FromLwssonTw0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  task2FromLwssonTw0
+//
+//  Created by vladimir gennadievich on 11.07.2020.
+//  Copyright © 2020 Vladimir Gennadievich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
