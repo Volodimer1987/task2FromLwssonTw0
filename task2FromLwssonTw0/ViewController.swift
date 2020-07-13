073eeb1d6936e9e0c1b1eeed956f8070a756cd57
Vladimir <kynicin87@mail.ru>
2020-07-14 00:06:36 +0300

    до слайдера

--- task2FromLwssonTw0/ViewController.swift
+++ task2FromLwssonTw0/ViewController.swift
@@ -17,12 +17,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // all about label adjust
         mainLabel.text = ""
         mainLabel.font = mainLabel.font.withSize(35)
+        mainLabel.textAlignment = .center
+        mainLabel.numberOfLines = 2
+        
+        // all adjust segmented control
+        segmantedControl.insertSegment(withTitle: "Thierd", at: 2, animated:false )
+         
     }
 
     @IBAction func changeSegmentInSegmentedControl() {
-        
+        switch segmantedControl.selectedSegmentIndex {
+        case 0:
+            mainLabel.text = "The first segment is selected"
+            mainLabel.textColor = .brown
+        case 1:
+            mainLabel.text = "The second segment is selected"
+            mainLabel.textColor = .blue
+        case 2:
+            mainLabel.text = "The thierd segment is selected"
+            mainLabel.textColor = .purple
+        default:
+            break
+        }
     }
     
 }
